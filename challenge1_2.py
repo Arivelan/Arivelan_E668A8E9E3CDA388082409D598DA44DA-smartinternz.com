a1e5443dd62a372d3fc01d7c6e0fd76203c93c48
Arivelan <144311696+Arivelan@users.noreply.github.com>
2023-09-08 11:05:25 +0530

    Update challenge1_2.py

--- challenge1_2.py
+++ challenge1_2.py
@@ -4,10 +4,9 @@
 year=int(input("Enter the year:"))
 
 #check whether is leap or not using if-elif-else statement
-if year%4==0 and year%100!=0:
+if (year%4==0 and year%100!=0) or (year%400==0):
   print("The {} year is leap year".format(year))
-elif year%400==0:
-  print("The {} year is leap year ".format(year))
+
 else:
   print("The {} year is not leap year ".format(year))
   
